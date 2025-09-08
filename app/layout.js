46c3e89b2dc9160dd44baac7af4731c46e02eaba
Justin Monserrat <107101778+justinMonserrat@users.noreply.github.com>
2025-09-08 14:35:11 -0400

    Fix mobile scroll issue

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
-        <Suspense fallback={null}>
+        <Suspense fallback={<div>Loading...</div>}>
           <Navbar />
         </Suspense>
         {children}
